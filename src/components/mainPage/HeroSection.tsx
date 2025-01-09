fd499d51132b4711003f5b232421f20529e2e378
Bernardo Cerqueira <katanardo@gmail.com>
2025-01-09 19:27:18 -0300

    Hero Section
    Adicionei a logo e o offcanvas. Também alterei alguns estilos e fiz a responsividade da seção.

--- src/components/mainPage/HeroSection.tsx
+++ src/components/mainPage/HeroSection.tsx
@@ -1,15 +1,37 @@
-import { Box } from "@radix-ui/themes";
 import styles from "./HeroSection.module.scss"
-import { Button } from "reactstrap";
+import { Box } from "@radix-ui/themes";
+import { Button, Offcanvas, OffcanvasBody, OffcanvasHeader } from "reactstrap";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useState } from "react";
+import { IoMenuOutline } from "react-icons/io5";
 
 export default function HeroSection(){
+    const [isOpen, setIsOpen] = useState(false)
+    const router = useRouter()
+
+    function handleClick(){
+        router.push("/")
+    }
+
+    const toggle = () => setIsOpen(!isOpen)
+
     return(
         <>
+            <Link href={"/"}>
+                <img src="/logo.png" alt="Logo" className={styles.logo}/>
+            </Link>
+            <Button
+                    className={styles.offcanvasButton}
+                    onClick={toggle}
+                >
+                    <IoMenuOutline className={styles.offcanvasIcon}/>
+            </Button>
             <Box className={styles.background}>    
                 <Box className={styles.blueDiv}>
                     <h1>Bernardo<br/>Cerqueira</h1>
                     <p>Eu sou um Desenvolvedor Web apaixonado por criar sites.</p>
-                    <Button>Conhecer Mais</Button>
+                    <Button onClick={handleClick}>Conhecer Mais</Button>
                 </Box>
                 <Box className={styles.greenDiv}> 
                         <img 
@@ -19,6 +41,23 @@ export default function HeroSection(){
                         />
                 </Box>
             </Box>
+
+            <div>
+                <Offcanvas
+                    isOpen={isOpen}
+                    toggle={toggle}
+                    direction="end"
+                >
+                    <OffcanvasHeader toggle={toggle}>
+                        Offcanvas
+                    </OffcanvasHeader>
+                    <OffcanvasBody>
+                        <strong>
+                            This is the Offcanvas body.
+                        </strong>
+                    </OffcanvasBody>
+                </Offcanvas>
+            </div>
         </>
     )
 }
