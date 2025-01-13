4c72f754dca218da463feac36a0a0f5e9d48206e
Bernardo Cerqueira <katanardo@gmail.com>
2025-01-13 17:18:59 -0300

    Footer + Estilização comum para toda page
    Essa estilização faz com que o footer fique fixado na parte de baixo do site

---
+++ src/components/common/CommonFooter/index.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+import styles from "./CommonFooter.module.scss"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+import { MdEmail } from "react-icons/md"
+
+export default function CommonFooter(){
+    const date = new Date()
+    const year = date.getFullYear()
+
+    return(
+        <>
+            <footer className={styles.footer}>
+                <p className={styles.copyright}>
+                    &copy; Bernardo Cerqueira {year}
+                </p>
+                <div className={styles.socialLinks}>
+                    <a href="mailto:becerqueira.dev@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <MdEmail />
+                    </a>
+                    <a href="https://github.com/BernardoCerqueira" target="_blank" rel="noopener noreferrer">
+                        <FaGithub />
+                    </a>
+                    <a href="https://www.linkedin.com/in/bernardo-cerqueira-dev/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin />
+                    </a>
+                </div>
+            </footer>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
+import styles from "@/styles/common/pages.module.scss"
 import CommonHeader from "@/components/common/CommonHeader";
+import CommonFooter from "@/components/common/CommonFooter"
 import HeroSection from "@/components/mainPage/HeroSection";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.page}>
       <CommonHeader/>
-      <HeroSection/>
-    </>
+      <main className={styles.main}>
+        <HeroSection/>
+      </main>
+      <CommonFooter/>
+    </div>
   );
 }
