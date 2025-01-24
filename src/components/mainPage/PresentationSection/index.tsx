af50c48d155901dad39fd95cb9ba1cb4d77a3ae7
Bernardo Cerqueira <katanardo@gmail.com>
2025-01-24 16:08:34 -0300

    Refatorando o texto da seção de apresentação

--- src/components/mainPage/PresentationSection/index.tsx
+++ src/components/mainPage/PresentationSection/index.tsx
@@ -12,9 +12,6 @@ export default function PresentationSection() {
                         Olá! Sou um desenvolvedor fullstack apaixonado por tecnologia e em busca da minha primeira oportunidade profissional na área. Recentemente, concluí o curso <span className={styles.highlight}>JavaScript Fullstack</span> da Onebitcode, no qual adquiri uma base sólida em desenvolvimento web e trabalhei em diversos projetos que demonstram minhas habilidades práticas <Link href={"#projects"} className={styles.link}><span className={styles.highlight}>(confira na seção de projetos)</span></Link>.
                     </p>
                     <p className={styles.aboutMeText}>
-                        Meu processo de trabalho é guiado pela busca por soluções inteligentes e bem planejadas. Antes de codar, gosto de analisar o problema, pensar nas melhores alternativas e só então implementar a solução. Isso reflete meu compromisso com a qualidade e a eficiência.
-                    </p>
-                    <p className={styles.aboutMeText}>
                         Quando não estou programando, gosto de estudar, aprender novas línguas, praticar esportes, jogar videogames e ouvir música. Essas atividades me ajudam a manter um equilíbrio entre trabalho e lazer, além de inspirar minha criatividade no desenvolvimento.
                     </p>
                 </Container>
@@ -33,7 +30,7 @@ export default function PresentationSection() {
                         No front-end, domino frameworks como <span className={styles.highlight}>React e Next.js</span>, construindo interfaces modernas e responsivas. No back-end, meu foco é em <span className={styles.highlight}>Node.js e Express.js</span>, com integração a bancos de dados <span className={styles.highlight}>PostgreSQL via Prisma</span>.
                     </p>
                     <p className={styles.techStackText}>
-                        Tenho interesse tanto no front-end quanto no back-end, com um desejo especial de explorar mais o mundo dos bancos de dados. Meu objetivo é crescer constantemente como desenvolvedor, aprendendo com profissionais experientes e contribuindo para projetos impactantes. Estou animado para começar minha jornada profissional e contribuir para o sucesso de uma equipe que valorize o meu crescimento e o meu aprendizado.
+                        Tenho interesse tanto no front-end quanto no back-end, com um desejo especial de explorar mais o mundo dos bancos de dados. Meu objetivo é crescer constantemente como desenvolvedor, aprendendo com profissionais experientes e contribuindo para projetos impactantes.
                     </p>
                 </Container>
                 <Container className={styles.techStackImageDiv}>
