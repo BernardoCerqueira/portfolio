d518added340382fbdca914de0c5f482a47aff2e
Bernardo Cerqueira <katanardo@gmail.com>
2025-01-29 18:27:33 -0300

    Lógica e estlização da lista de tecnologias

--- src/components/projectPage/ProjectSession/index.tsx
+++ src/components/projectPage/ProjectSession/index.tsx
@@ -6,7 +6,7 @@ type props = {
     name: string
     link: string
     textLink: string
-    techList: {backend?: string, frontend?: string, general?: string}
+    techList: { backend?: string, frontend?: string, general?: string }
 }
 
 export default function ProjectSession({ name, link, textLink, techList }: props) {
@@ -18,25 +18,44 @@ export default function ProjectSession({ name, link, textLink, techList }: props
                 target="_blank"
                 className={styles.link}
             >
-                    {textLink}
+                {textLink}
             </a>
             <div className={styles.carousel}>
                 [CARROSSEL AQUI]
             </div>
             <div className={styles.techList}>
-                <p>Tecnologias utilizadas:</p>
-                <p>
-                    {techList.frontend ? "Frontend: " + techList.frontend : null}
-                </p> 
-                <p>
-                    {techList.backend ? "Backend: " + techList.backend : null}
-                </p>
-                <p>
-                    {techList.general ? techList.general : null}
-                </p>
+                <div>
+                    <span className={styles.techListTitle}>Tecnologias utilizadas: </span>
+                    {techList.general
+                        ? techList.general
+                        : null
+                    }
+                </div>
+                <div className={styles.techListItem}>
+                    {techList.frontend
+                        ?
+                        <p>
+                            <span className={styles.techListTitle}>
+                                Frontend -
+                            </span> {techList.frontend}
+                        </p>
+                        : null
+                    }
+                </div>
+                <div className={styles.techListItem}>
+                    {techList.backend
+                        ?
+                        <p>
+                            <span className={styles.techListTitle}>
+                                Backend -
+                            </span> {techList.backend}
+                        </p>
+                        : null
+                    }
+                </div>
             </div>
             <div className={styles.description}>
-                <ReadMe projectName={name}/>
+                <ReadMe projectName={name} />
             </div>
         </div>
     )
