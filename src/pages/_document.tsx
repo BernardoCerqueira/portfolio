fb57d993ea8261579f26f7dbfaada41bf8fc9e67
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-13 17:41:27 -0300

    Google Head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
+      <head>
+        <meta name="google-site-verification" content="lS2OeaZM45pA23oBc541VTRXEpLoATd7FChsVTgrSPU" />
+      </head>
       <body>
         <Main />
         <NextScript />
