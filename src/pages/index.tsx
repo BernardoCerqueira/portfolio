3f9cc81a55bb4c9d3ed839ccc9cf375dc53e2d54
Bernardo Cerqueira <katanardo@gmail.com>
2025-02-14 17:38:03 -0300

    Google and meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Bernardo Cerqueira | Currículo</title>
+        <title>Bernardo Cerqueira | Desenvolvedor Web e Programador</title>
         <meta name="description" content="Confira o currículo do Bernardo Cerqueira Marques e saiba mais sobre a sua carreira e suas habilidades." />
 
-        <meta property="og:title" content="Bernardo Cerqueira | Currículo" />
+        <meta property="og:title" content="Bernardo Cerqueira | Desenvolvedor Web e Programador" />
         <meta property="og:description" content="Confira o currículo do Bernardo Cerqueira Marques e saiba mais sobre a sua carreira e suas habilidades." />
-        <meta property="og:url" content="https://becerqueira.com/curriculum" />
-        <meta property="og:image" content="https://becerqueira.com/programmer-art.svg" />
+        <meta property="og:url" content="https://www.becerqueira.com" />
+        <meta property="og:image" content="https://www.becerqueira.com/programmer-art.svg" />
         <meta property="og:image:alt" content="Bê Cerqueira" />
         <meta property="og:type" content="website" />
 
